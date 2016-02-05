69e3e10e9168137eb9eb59d0ee978bbba794de50
Jonah <jonah.stieglitz17@gmail.com>
2016-02-05 11:45:33 -0600

    Start of something cool...

---
+++ src/main/java/frames/MainFrame.java
@@ -0,0 +1,19 @@
+package frames;
+
+/*
+ * Created by Jonah on 2/5/2016.
+ */
+
+public class MainFrame
+{
+
+    public MainFrame()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Let's Get It");
+    }
+}
